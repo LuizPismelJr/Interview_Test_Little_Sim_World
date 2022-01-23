99a580f172cab7306a97b8300863da02a64d5251
LuizPismelJr <61069801+LuizPismelJr@users.noreply.github.com>
2022-01-23 08:30:27 -0300

    ShopKeeper Interaction Teste

---
+++ Assets/_Scripts/Npcs/ShopKeeper/ShopKeeperInteract.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopKeeperInteract : MonoBehaviour, IInteractable
+{
+   public void Interact() 
+    {
+        Debug.Log("Vai se fuder Douzane");
+    }
+}
